4c5ebdb744fb894865fda65b65fa315aca034e75
Abdykarim.D <Abdykarim.D@magnum.kz>
2023-06-26 18:18:46 +0600

    1П начало

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import sys
 
 from config import project_path, project_name
-from core import init_logger
+
 
 if __name__ == '__main__':
-    # ? LOGGER
-    if len(sys.argv) == 1:
-        sys.argv.append('dev')
-    logger = init_logger(tg_chat_id='531139435', orc_file_path=project_path.joinpath(f'{sys.argv[1]}.log'))
-    logger.warning(project_name)
+    print('kek')
